f7b7b94f518922ff0435ecee47fe2c164a3d4c6a
xScottishKrissx <chrisdunne66@gmail.com>
2017-12-11 16:28:10 +0000

    Removed Caching Temporarily

---
+++ arrays.php
@@ -0,0 +1,14 @@
+<?php
+	// Nav Items
+	$navItems = array(
+					array(
+						slug => "index.php",
+						title => "Home"
+					),
+					array(
+						slug => "team.php",
+						title => "Team"
+					),
+			);
+
+?>
---
+++ column2.php
@@ -0,0 +1,23 @@
+		<div id="column2">
+			
+			<div id="quickNav">
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+				
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+				<img src="https://place-hold.it/140x140" />
+			</div>
+			
+			<div id="banners">				
+				<img src="img/mini_livethelegend.jpg" />				
+				<img src="img/celtictvbanner.jpg" />				
+				<img src="img/filmadbanner.jpg" />				
+				<img src="img/mini_4matchpackage.jpg" />				
+				<img src="img/mini_bookad.jpg" />				
+				<img src="img/mini_mrgeen.jpg" />
+			</div>
+		</div>
