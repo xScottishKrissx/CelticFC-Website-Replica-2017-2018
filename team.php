1ffb180554dd2598de32e67ebbfabd7c49168351
xScottishKrissx <chrisdunne66@gmail.com>
2017-12-11 18:11:11 +0000

    End of Work Day
    Added Content to team pages and disabled caching in xampp

--- team.php
+++ team.php
@@ -12,30 +12,175 @@
 				
 				
 				<div class="goalkeepers">
-					<h2>GoalKeepers</h2>
-					<img src="img/playerImages/craigGordon.jpg" />
-					<img src="img/playerImages/craigGordon.jpg" />
-					<img src="img/playerImages/craigGordon.jpg" />
+					<h2>GoalKeepers</h2>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/craigGordon.jpg" />
+						<div class="playerName"><span>1. C. Gordon</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/devries.jpg" />
+						<div class="playerName"><span>24. D. De Vries</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/hazard.jpg" />
+						<div class="playerName"><span>65. C. Hazard</span></div>
+					</div>
 				</div>
 				
 				<div class="defenders">
 					<h2>Defenders</h2>					
-					<img src="img/playerImages/craigGordon.jpg" />	
-					<img src="img/playerImages/craigGordon.jpg" />
-					<img src="img/playerImages/craigGordon.jpg" />
-					<img src="img/playerImages/craigGordon.jpg" />	
-					<img src="img/playerImages/craigGordon.jpg" />
-					<img src="img/playerImages/craigGordon.jpg" />			
+					
+					<div class="playerItem">
+						<img src="img/playerImages/simunovic.jpg" />
+						<div class="playerName"><span>5. J. Simunovic</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/gamboa.jpg" />
+						<div class="playerName"><span>12. C. Gamboa</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/boyata.jpg" />
+						<div class="playerName"><span>20. D. Boyata</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/lustig.jpg" />
+						<div class="playerName"><span>23. M. Lustig</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/sviatchenko.jpg" />
+						<div class="playerName"><span>28. E. Sviatchenko</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/ajer.jpg" />
+						<div class="playerName"><span>35. K. Ajer</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/ralston.jpg" />
+						<div class="playerName"><span>56. A. Ralston</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/miller.jpg" />
+						<div class="playerName"><span>59. C. Miller</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/tierney.jpg" />
+						<div class="playerName"><span>63. K. Tierney</span></div>
+					</div>		
 				</div>
 				
-				<div class="midfielders">
-					<h2>Defenders</h2>					
-					<img src="img/playerImages/craigGordon.jpg" />
+				<div class="midfielders">					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/bitton.jpg" />
+						<div class="playerName"><span>6. N. Bitton</span></div>
+					</div>					
+					
+					<div class="playerItem">
+						<img src="img/playerImages/roberts.jpg" />
+						<div class="playerName"><span>7. P. Roberts</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/brown.jpg" />
+						<div class="playerName"><span>8. S. Brown</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/sinclair.jpg" />
+						<div class="playerName"><span>11. S. Sinclair</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/armstrong.jpg" />
+						<div class="playerName"><span>14. S. Armstrong</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/hayes.jpg" />
+						<div class="playerName"><span>15. J. Hayes</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/rogic.jpg" />
+						<div class="playerName"><span>18. T. Rogic</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/ntcham.jpg" />
+						<div class="playerName"><span>21. O. Ntcham</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/benyu.jpg" />
+						<div class="playerName"><span>26. K. Benyu</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/mcgregor.jpg" />
+						<div class="playerName"><span>42. C. McGregor</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/forrest.jpg" />
+						<div class="playerName"><span>49. J. Forrest</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/henderson.jpg" />
+						<div class="playerName"><span>53. L. Henderson</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/kouassi.jpg" />
+						<div class="playerName"><span>88. E. Kouassi</span></div>
+					</div>		
 				</div>
 				
-				<div class="forwards">
-					<h2>Defenders</h2>					
-					<img src="img/playerImages/craigGordon.jpg" />
+				<div class="forwards">						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/griffiths.jpg" />
+						<div class="playerName"><span>9. L. Griffiths</span></div>
+					</div>							
+					
+					<div class="playerItem">
+						<img src="img/playerImages/dembele.jpg" />
+						<div class="playerName"><span>10. M. Dembele</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/edouard.jpg" />
+						<div class="playerName"><span>22. O. Edouard</span></div>
+					</div>
+				</div>
+				
+				<div class="loan">						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/christie.jpg" />
+						<div class="playerName"><span>17. R. Christie</span></div>
+					</div>							
+					
+					<div class="playerItem">
+						<img src="img/playerImages/allan.jpg" />
+						<div class="playerName"><span>19. S. Allan</span></div>
+					</div>						
+					
+					<div class="playerItem">
+						<img src="img/playerImages/ciftci.jpg" />
+						<div class="playerName"><span>N. Ciftci</span></div>
+					</div>
 				</div>
 		
 		
