9d277c65c3e4d9fc9c95f1781f22a2dccaab20a2
xScottishKrissx <chrisdunne66@gmail.com>
2017-12-14 17:48:33 +0000

    Player Profile Page Commit
    Have most of the player profile page up and running. Just need to fix
    the name and then start populating the database.

--- playerprofile.php
+++ playerprofile.php
@@ -1,7 +1,5 @@
 <?php	include ('header.php'); ?>
 
-<div >
->
 
 <?php 
     $player = $_GET["name"];
@@ -13,18 +11,60 @@
 	
 	while ($row = $result->fetch(PDO::FETCH_ASSOC)){
 		
+		$squad_num = $row['squad_num'];
 		$name = $row['name'];
 		$position = $row['position'];
+		
+		$born = $row['born'];
+		$signed  = $row['signed'];
+		$starts  = $row['starts'];
+		$sub_appearances  = $row['sub_appearances'];
+		$total_appearances  = $row['total_appearances'];
+		$nationality  = $row['nationality'];
+		$caps  = $row['caps'];
+		$imagepath  = $row['imagepath'];
 	}
 	
 	
 	?>
 
-<div>
+<div class="playerProfilePageContent">
+	<img src="img/playerProfileImages/<?php echo $imagepath;  ?>" />
 	
-	<?php echo $name; echo $position; ?>
+	<div class="playerPositionInfo">
+		
+		<div class="squadNumContainer">
+			<p id="squadNum"><?php echo $squad_num; ?></p>
+		</div>
+		
+		<div class="nameandposition">
+			<p id="name"><?php echo $name;?></p>
+			<p id="position"><?php echo $position;?></p>
+		</div>
+	</div>
+	
+	<div class="playerStats">
+		<div class="leftTableColumn">
+			<p>Born</p>
+			<p>Signed</p>
+			<p>Starts</p>
+			<p>Sub Appearances</p>
+			<p>Total Appearances</p>
+			<p>Nationality</p>
+			<p>Caps</p>
+		</div>
+		<div class="rightTableColumn">
+			<p><?php echo $born; ?> </p>
+			<p><?php echo $signed; ?> </p>
+			<p><?php echo $starts; ?> </p>
+			<p><?php echo $sub_appearances; ?> </p>
+			<p><?php echo $total_appearances; ?> </p>
+			<p><?php echo $nationality; ?> </p>
+			<p><?php echo $caps; ?> </p>
+		</div>
+	</div>
 </div>
-</div>
+
 
 
 
